010e33f6e259f9956502c1c46923671fa20b2d95
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-15 00:36:45 -0500

    feat(environments): add courses endpoint path.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  // Server Base URL for Fake REST API
+  serverBaseUrl: 'http://localhost:3600/api/v1',
+  usersEndpointPath: '/users',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  // Server Base URL for REST API
+  serverBaseUrl: 'http://localhost:3600/api/v1',
+  usersEndpointPath: '/users',
+};

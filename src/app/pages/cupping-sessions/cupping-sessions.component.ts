929a31cf40f91f76ef7aba63be7fc78f518ede43
Guillermo Tantaleán <guillermo.rs2017@gmail.com>
2025-05-14 21:31:07 -0500

    feat(public): add cupping sessions component structure

---
+++ src/app/pages/cupping-sessions/cupping-sessions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cupping-sessions',
+  imports: [],
+  templateUrl: './cupping-sessions.component.html',
+  styleUrl: './cupping-sessions.component.css'
+})
+export class CuppingSessionsComponent {
+
+}

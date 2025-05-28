8c06f29a751ddb85eec6c110781b5a1e4f28799a
Guillermo Tantaleán <guillermo.rs2017@gmail.com>
2025-05-28 14:57:47 -0500

    feat: import pages from inventory feature branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import {ViewFileComponent} from './consultations/pages/view-file/view-file.compo
 import {ViewNewDefectComponent} from './consultations/pages/view-new-defect/view-new-defect.component';
 import {WelcomeBaristaComponent} from './dashboards/pages/welcome-barista/welcome-barista.component';
 import {SesionesCataComponent} from './cupping-sessions/pages/sesiones-cata/sesiones-cata.component';
+import {InventaryComponent} from './inventory/pages/inventary.component';
 
 export const routes: Routes = [
   { path: 'libraryDefects',             component: ViewConsultationsComponent },
   { path: 'welcome-barista', component: WelcomeBaristaComponent },
   {path: 'cupping-sessions', component: SesionesCataComponent},
+  {path: 'inventory', component: InventaryComponent},
   { path: 'file/:id', component: ViewFileComponent },
   { path: 'new-defect', component: ViewNewDefectComponent },
   { path: '',                 redirectTo: 'libraryDefects', pathMatch: 'full' },

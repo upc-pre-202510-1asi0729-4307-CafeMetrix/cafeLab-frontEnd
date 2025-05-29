29dbd16ebfc0d9380797cba9ae7c18df8905e7b7
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-29 12:08:27 -0500

    feat(pages): modify edit profile page

--- src/app/auth/pages/edit-profile-page/edit-profile-page.component.ts
+++ src/app/auth/pages/edit-profile-page/edit-profile-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { EditProfileFormComponent } from '../../components/edit-profile-form/edit-profile-form.component';
+import { ToolbarinitComponent } from '../../../public/components/toolbarinit/toolbarinit.component';
 
 @Component({
   selector: 'app-edit-profile-page',
-  imports: [],
   templateUrl: './edit-profile-page.component.html',
-  styleUrl: './edit-profile-page.component.css'
+  styleUrls: ['./edit-profile-page.component.css'],
+  standalone: true,
+  imports: [EditProfileFormComponent, ToolbarinitComponent]
 })
-export class EditProfilePageComponent {
-
-}
+export class EditProfilePageComponent {}

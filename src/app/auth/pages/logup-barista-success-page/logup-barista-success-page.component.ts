233efa686a997b97584b6492694edb2c43acee77
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-29 11:13:44 -0500

    feat(pages): modify logup barista success page

--- src/app/auth/pages/logup-barista-success-page/logup-barista-success-page.component.ts
+++ src/app/auth/pages/logup-barista-success-page/logup-barista-success-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatButton} from '@angular/material/button';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-logup-barista-success-page',
-  imports: [],
   templateUrl: './logup-barista-success-page.component.html',
-  styleUrl: './logup-barista-success-page.component.css'
+  imports: [
+    MatButton,
+    TranslatePipe
+  ],
+  styleUrls: ['./logup-barista-success-page.component.css']
 })
 export class LogupBaristaSuccessPageComponent {
+  constructor(private router: Router) {}
 
+  onContinue() {
+    this.router.navigate(['/login']);
+  }
 }

f34a230fcf686149df3b0b76fff98f12b44a7a26
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-29 10:18:03 -0500

    feat(toolbarinit): remove unused toolbarinit imports

--- src/app/public/components/toolbarinit/toolbarinit.component.ts
+++ src/app/public/components/toolbarinit/toolbarinit.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
-import {TranslatePipe} from '@ngx-translate/core';
 import {LanguageSwitcherComponent} from '../language-switcher/language-switcher.component';
-import {MatAnchor} from '@angular/material/button';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-
 
 @Component({
   selector: 'app-toolbarinit',
   imports: [
     MatToolbar,
-    TranslatePipe,
-    LanguageSwitcherComponent,
-    MatAnchor,
-    RouterLink,
-    RouterLinkActive
+    LanguageSwitcherComponent
   ],
   templateUrl: './toolbarinit.component.html',
   styleUrl: './toolbarinit.component.css'
